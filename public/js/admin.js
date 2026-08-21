99ddcbb4dffc8e4f7679fd752363be7e866196ef
Rauha201 <rauhauddin1@gmail.com>
2026-08-22 01:42:00 +0600

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,6 +6,7 @@
 // here would mean fighting its DOM assumptions instead of just
 // writing a matching pair of files.
 
+
 const ADMIN_API = '/api/admins';
 
 const adminToken = localStorage.getItem('token');

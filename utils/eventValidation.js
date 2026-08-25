bb28fb6a3325d6e6f3e4ec8f978f181dedf3db30
Rauha201 <rauhauddin1@gmail.com>
2026-08-26 00:21:41 +0600

    Update eventValidation.js

--- utils/eventValidation.js
+++ utils/eventValidation.js
@@ -4,7 +4,6 @@
 // eventController.js used to have this inline, which meant testing
 // it dragged in the whole models/config chain.
 
-
 const CATEGORIES = ['Concert', 'Meetup', 'Workshop', 'Sports', 'Festival'];
 
 function todayDateString() {
